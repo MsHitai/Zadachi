488da59c6b132721bae370eb224793e436a4ef99
Трушанина Мария <mmalafeeva@yandex.ru>
2023-11-19 20:20:08 +0300

    feat: добавить задачу с leetcode на дистанцию

---
+++ src/ya_internship/Book.java
@@ -0,0 +1,23 @@
+package ya_internship;
+
+import java.io.*;
+import java.util.Objects;
+
+public class Book implements Serializable {
+    String title;
+    int publicationYear;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, publicationYear);
+    }
+
+}
