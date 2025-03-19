d2822b8e4b99cc5be3bcff9f5850283525615a4e
Трушанина Мария <m.trushanina@itq-group.com>
2025-03-19 12:30:19 +0300

    node lists ya course

--- src/ya/algorithm_course/first_sprint/TestTheory.java
+++ src/ya/algorithm_course/first_sprint/TestTheory.java
@@ -2,35 +2,80 @@ package ya.algorithm_course.first_sprint;
 
 // <template>
 class Node<V> {
-    public V value;
-    public Node<V> next;
 
-    public Node(V value, Node<V> next) {
+    public V value;
+
+    public Node<V> next;
+    public Node<V> prev;
+
+    public Node(V value, Node<V> next, Node<V> prev) {
         this.value = value;
         this.next = next;
+        this.prev = prev;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", next=" + next +
+                '}';
     }
 }
 // <template>
 
-public class TestTheory {
-    public static void solution(Node<String> head) {
-        // Your code
-        // ヽ(´▽`)/
-        System.out.println(head.value);
+ class TestTheory {
+
+     public static void main(String[] args) {
+         Node<String> node3 = new Node<>("node3", null, null);
+         Node<String> node2 = new Node<>("node2", node3, null);
+         Node<String> node1 = new Node<>("node1", node2, null);
+         Node<String> node0 = new Node<>("node0", node1, null);
+         node1.prev = node0;
+         node2.prev = node1;
+         node3.prev = node2;
+         Node<String> newNode = reverseTwoPointerLinkedList(node0);
+         System.out.println("newNode: " + newNode);
+         System.out.println("newNode.prev: " + newNode.prev);
+         System.out.println("newNode.next: " + newNode.next);
+         test();
+     }
+
+    public static Node<String> reverseTwoPointerLinkedList(Node<String> head) {
+        Node<String> current = head;
+        Node<String> temp = null;
+        Node<String> newHead = null;
+
+        while (current != null) {
+            // Save the next node before swapping pointers
+            temp = current.prev;
+            // Swap next and prev pointers
+            current.prev = current.next;
+            current.next = temp;
+            // Move to the next node in the original list
+            newHead = current; // This becomes the new head at the end
+            current = current.prev; // Use original next (now prev after swap)
+        }
+
+        return newHead;
     }
 
-    private static void test() {
-        Node<String> node3 = new Node<>("node3", null);
-        Node<String> node2 = new Node<>("node2", node3);
-        Node<String> node1 = new Node<>("node1", node2);
-        Node<String> node0 = new Node<>("node0", node1);
-        solution(node0);
-        /*
-        Output is:
-        node0
-        node1
-        node2
-        node3
-        */
+    public static void test() {
+        Node<String> node3 = new Node<>("node3", null, null);
+        Node<String> node2 = new Node<>("node2", node3, null);
+        Node<String> node1 = new Node<>("node1", node2, null);
+        Node<String> node0 = new Node<>("node0", node1, null);
+        node1.prev = node0;
+        node2.prev = node1;
+        node3.prev = node2;
+        Node<String> newNode = reverseTwoPointerLinkedList(node0);
+        /* result is :*/
+        System.out.println(newNode == node3);
+        System.out.println(node3.next == node2);
+        System.out.println(node2.next == node1);
+        System.out.println(node2.prev == node3);
+        System.out.println(node1.next == node0);
+        System.out.println(node1.prev == node2);
+        System.out.println(node0.prev == node1);
     }
 }
