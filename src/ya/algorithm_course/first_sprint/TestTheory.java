46210f408ef36e82fca6c2ebafc8bf14f8a1a268
Трушанина Мария <m.trushanina@itq-group.com>
2025-03-19 23:09:08 +0300

    stack type solutions ya course

--- src/ya/algorithm_course/first_sprint/TestTheory.java
+++ src/ya/algorithm_course/first_sprint/TestTheory.java
@@ -27,34 +27,20 @@ class Node<V> {
  class TestTheory {
 
      public static void main(String[] args) {
-         Node<String> node3 = new Node<>("node3", null, null);
-         Node<String> node2 = new Node<>("node2", node3, null);
-         Node<String> node1 = new Node<>("node1", node2, null);
-         Node<String> node0 = new Node<>("node0", node1, null);
-         node1.prev = node0;
-         node2.prev = node1;
-         node3.prev = node2;
-         Node<String> newNode = reverseTwoPointerLinkedList(node0);
-         System.out.println("newNode: " + newNode);
-         System.out.println("newNode.prev: " + newNode.prev);
-         System.out.println("newNode.next: " + newNode.next);
          test();
      }
 
     public static Node<String> reverseTwoPointerLinkedList(Node<String> head) {
         Node<String> current = head;
-        Node<String> temp = null;
+        Node<String> temp;
         Node<String> newHead = null;
 
         while (current != null) {
-            // Save the next node before swapping pointers
             temp = current.prev;
-            // Swap next and prev pointers
             current.prev = current.next;
             current.next = temp;
-            // Move to the next node in the original list
-            newHead = current; // This becomes the new head at the end
-            current = current.prev; // Use original next (now prev after swap)
+            newHead = current;
+            current = current.prev;
         }
 
         return newHead;
---
+++ src/ya/algorithm_course/second_sprint/StackMax.java
@@ -0,0 +1,108 @@
+package ya.algorithm_course.second_sprint;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public class StackMax {
+
+    private final List<Integer> items;
+    private final Deque<Integer> sorted;
+
+    public StackMax(int length) {
+        items = new ArrayList<>(length);
+        sorted = new LinkedList<>();
+    }
+
+    public void push(Integer item) {
+        if (!sorted.isEmpty() && item >= sorted.getLast()) {
+            sorted.add(item);
+            items.add(item);
+            return;
+        }
+        items.add(item);
+        sorted.addFirst(item);
+    }
+
+    public Integer pop() {
+        if (items.isEmpty()) {
+            return Integer.MIN_VALUE;
+        } else if (Objects.equals(items.get(size() - 1), sorted.getLast())) {
+            sorted.removeLast();
+            return items.remove(items.size() - 1);
+        }
+        Integer temp = items.remove(items.size() - 1);
+        sorted.remove(temp);
+        return temp;
+    }
+
+    public Integer top() {
+        if (items.isEmpty()) {
+            return Integer.MIN_VALUE;
+        } else {
+            return items.get(items.size() - 1);
+        }
+    }
+
+    public Integer getMax() {
+        if (items.isEmpty()) {
+            return Integer.MIN_VALUE;
+        } else {
+            return sorted.getLast();
+        }
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+            int length = Integer.parseInt(reader.readLine());
+            String[] commands = new String[length];
+            for (int i = 0; i < length; i++) {
+                commands[i] = reader.readLine();
+            }
+            StackMax stackMax = new StackMax(length);
+            processCommands(commands, stackMax);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void processCommands(String[] commands, StackMax stackMax) {
+        int temp;
+        for (String command : commands) {
+            if (command.startsWith("get_max")) {
+                temp = stackMax.getMax();
+                if (temp == Integer.MIN_VALUE) {
+                    System.out.println("None");
+                } else {
+                    System.out.println(temp);
+                }
+            } else if (command.startsWith("push")) {
+                String[] split = command.split(" ");
+                temp = Integer.parseInt(split[1]);
+                stackMax.push(temp);
+            } else if (command.startsWith("top")) {
+                temp = stackMax.top();
+                if (temp == Integer.MIN_VALUE) {
+                    System.out.println("error");
+                } else {
+                    System.out.println(temp);
+                }
+            } else {
+                temp = stackMax.pop();
+                if (temp == Integer.MIN_VALUE) {
+                    System.out.println("error");
+                }
+            }
+        }
+    }
+}
